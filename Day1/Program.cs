2811437d6fd8f1fe71d65bb0c50a420ae53f7d36
Snuzmumrik <jakob.krantz92@gmail.com>
2019-12-01 21:53:24 +0100

    Satt upp projekt, stulit dag1 skamlöst av bernie

---
+++ Day1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Day1
+{
+    class Day1
+    {
+        static void Main(string[] args)
+        {
+            var fuelReq = 0;
+            using (StreamReader input = File.OpenText("./input.txt"))
+            {
+                while (!input.EndOfStream)
+                {
+                    var line = input.ReadLine();
+                    int.TryParse(line, out var mass);
+                    fuelReq += (int)Math.Floor(mass / 3f) - 2;
+
+                }
+            }
+
+            Console.WriteLine(fuelReq);
+        }
+    }
+}
--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace AdventOfCode2019
+namespace Day2
 {
     class Program
     {
